ce1910a72d794d62e6b22163fb9f7a3136dfaceb
Eitan Mor <eitan.mor@education.nsw.gov.au>
2025-03-25 21:29:03 +1100

    Added many features, window finally responds save

--- assesment bugs eyes.py
+++ assesment bugs eyes.py
@@ -4,37 +4,77 @@ pygame.init()
  
 # Colours
 BACKGROUND = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
  
 # Game Setup/Global variables
 FPS = 60
 fpsClock = pygame.time.Clock()
-WINDOW_WIDTH = 1500
-WINDOW_HEIGHT = 1200
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 800
+global score
 score = 0
+
  
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Beat the Bug Eyes!')
  
-# Function definitions
-def findPosition (int) :
+## Function definitions
 
-
+# Function for drawing the eyes in a random spot
+def drawEyes (int) :
   if int == 1 :
-    down = 5
-    across = 1
+    down = random.uniform(5, 250)
+    across = random.uniform(5, 350)
   elif int == 2 :
-    down = 1
-    across = 9
+    down = random.uniform(5, 250)
+    across = random.uniform(550, 900)
   elif int == 3 :
-    down = 5
-    across = 18
+    down = random.uniform(400, 700)
+    across = random.uniform(5, 350)
   else :
-    down = 10
-    across = 7
-  return (down, across)
+    down = random.uniform(400, 700)
+    across = random.uniform(550, 900)
+  eye1 = pygame.Rect(across, down, 40 ,70)
+  eye2 = pygame.Rect(across + 50, down, 40 ,70)
+  pupil1 = pygame.Rect(across, down + 30, 40 ,10)
+  pupil2 = pygame.Rect(across + 50, down + 30, 40 ,10)
+  pygame.draw.ellipse(WINDOW, RED, eye1)
+  pygame.draw.ellipse(WINDOW, RED, eye2)
+  pygame.draw.ellipse(WINDOW, GREEN, pupil1)
+  pygame.draw.ellipse(WINDOW, GREEN, pupil2)
+  pygame.display.update()
+  fpsClock.tick(FPS)
 
-def drawEyes (position) :
-  return ()
+# Function for looking for an input from the player
+def inputLoop(rand) :
+  pygame.display.update()
+  fpsClock.tick(FPS)
+  keyPressed = False
+  for event in pygame.event.get() :
+    while keyPressed == False :
+      pygame.event.pump()
+      pressed = pygame.key.get_pressed()
+      initialTime = time.time()
+      if rand == 1 and pressed[K_LEFT] and pressed[K_UP] :
+        timePressed = time.time()
+        keyPressed = True
+      elif rand == 2 and pressed[K_RIGHT] and pressed[K_UP] :
+        timePressed = time.time()
+        keyPressed = True
+      elif rand == 3 and pressed[K_LEFT] and pressed[K_DOWN] :
+        timePressed = time.time()
+        keyPressed = True
+      elif rand == 4 and pressed[K_RIGHT] and pressed[K_DOWN] :
+        timePressed = time.time()
+        keyPressed = True
+  pygame.display.update()
+  fpsClock.tick(FPS)
+  #print (keyPressed) DOS
+  return(timePressed-initialTime)
+
+        
+  
 
 
 
@@ -42,32 +82,51 @@ def drawEyes (position) :
 def main () :
   looping = True
   turn = 1
-
   # The main game loop
   while looping :
     # Get inputs
     for event in pygame.event.get() :
       if event.type == QUIT :
         looping = False
- 
-    # Render elements of the game
-    WINDOW.fill(BACKGROUND)
-    pygame.display.update()
-    fpsClock.tick(FPS)
 
     # Game progression
     print ("BUG EYES")
+
     # set turns
     while turn <= 10 :
+      pygame.event.get()
+      # Render elements of the game
+      WINDOW.fill(BACKGROUND)
+      pygame.display.update()
+      fpsClock.tick(FPS)
       # Delay game for a random time between 2 and 4 seconds
       time.sleep(random.uniform(2,4))
       #print(turn) DOS
+      #rand = 3 DOS
+      
+      # Assigns a random integer from 1 to 4 for which quadrant the eyes are going to be in then draws them
       rand = random.randint(1, 4)
-      position = findPosition(rand)
-      drawEyes(position)
+      drawEyes(rand)
 
+      # Asks for player input
+      #print (f"rand: {rand}") DOS
+      inputLoop(rand)
 
+      # Displays the player score so far
+      print (f"score: {score}")
       turn += 1
+    
+    
+    # Displays final score and closing message
+    print (f"Final score: {score}")
+    if score > 1000 :
+      print ("Congratulations for saving your planet!")
+    else :
+      print ("...")
+      time.sleep(1)
+      print ("You let everyone die. Despicable")
+      # Quit game
     pygame.quit()
     quit()
+
 main()
